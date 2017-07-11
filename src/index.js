b1e6acd476a5e3f57d6121259d44e4cee24e5d4e
Aarni Koskela <akx@iki.fi>
2017-07-11 16:58:39 +0200

    embutter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
 import {rotate, dist, normalize} from './math';
+import colors from './palette';
+import makeGradient from './gradient';
 require('./style.css');
 
-const colors = [
-	'#06d6a0',
-	'#118ab2',
-	'#2274a5',
-	'#2a9d8f',
-	'#32936f',
-	'#348aa7',
-	'#525174',
-	'#5bc0eb',
-	'#5dd39e',
-	'#9bc53d',
-	'#bce784',
-	'#e55934',
-	'#e76f51',
-	'#e83f6f',
-	'#e9c46a',
-	'#ef476f',
-	'#f4a261',
-	'#f71735',
-	'#fa7921',
-	'#fde74c',
-	'#ffbf00',
-	'#ffd166',
-];
-const strokeColor = colors[Math.floor(Math.random() * colors.length)];
+
+const color1 = colors[Math.floor(Math.random() * colors.length)];
+const color2 = colors[Math.floor(Math.random() * colors.length)];
+const gradient = makeGradient([color1, color2], 800);
 
 const canvas = Object.assign(document.createElement('canvas'), {
 	width: 800,
@@ -46,9 +27,14 @@ function translateCoords(e) {
 
 canvas.addEventListener('mousedown', (e) => {
 	const {x, y} = translateCoords(e);
+	console.log(e);
 	ats.forEach((at) => {
 		if(dist(x, y, at.x, at.y) < 0.05) {
 			currentAt = at;
+			if(e.metaKey) {
+				currentAt.s *= -1;
+				clearNext = true;
+			}
 		}
 	});
 });
@@ -85,17 +71,17 @@ function applyAt({x, y, s}, at) {
 	return {
 		x: x + strength * norm.x,
 		y: y + strength * norm.y,
-		s: s + Math.abs(strength),
+		s: s + strength,
 	};
 }
 
 
-const rot = Math.random() * Math.PI * 2;
+let rot = Math.random() * Math.PI * 2;
 const radA = 0.3 + Math.random() * 0.7;
 const radB = 0.3 + Math.random() * 0.7;
 
 const ats = [];
-for(var i = 0; i < Math.random() * 3; i++) {
+for(var i = 0; i < Math.random() * 25; i++) {
 	ats.push(genAt());
 }
 
@@ -111,23 +97,45 @@ function compute(t) {
 }
 
 let dStep = 0;
+const colorIdxMul = 150;
 
-function step(t0=0) {
+function step() {
 	const trans = canvas.width / 2;
 	if(clearNext) {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		clearNext = false;
 		dStep = 0;
+	} else {
+		/*
+		rot += 0.01;
+		ctx.globalAlpha = 0.02;
+		ctx.fillStyle = 'white';
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		*/
 	}
 
-	ctx.fillStyle = strokeColor;
-	ctx.globalAlpha = 0.08;
-	for(var i = 0; i < 500; i++) {
+	ctx.globalAlpha = 0.9;
+	const t0 = +new Date();
+	for(var i = 0; i < 50000; i++) {
 		const pt = compute((dStep++)/100.);
 		ctx.beginPath();
-		const r = pt.s * 15;
-		ctx.ellipse(trans + pt.x * trans, trans + pt.y * trans, r, r, 0, 0, 6.283, false);
+		const r = 2 + Math.abs(pt.s * 15);
+		/*const colorIdx = Math.floor(
+			Math.min(colorIdxMul, Math.abs(pt.s * colorIdxMul)) / colorIdxMul * gradient.length
+		);
+		*/
+		const colorIdx = Math.floor((pt.y + 1) / 2 * gradient.length);
+		ctx.fillStyle = gradient[colorIdx];
+		ctx.ellipse(trans + pt.x * trans, trans + pt.y * trans, r, r, 0, 0, 6.283, false);		
 		ctx.fill();
+		if(true) {
+			ctx.beginPath();
+			ctx.ellipse(trans - pt.x * trans, trans + pt.y * trans, r, r, 0, 0, 6.283, false);
+			ctx.fill();
+		}
+
+		const t = (+new Date()) - t0;
+		if(t >= 10) break;
 	}
 	ctx.globalAlpha = 1;
 	ctx.lineWidth = 0;
